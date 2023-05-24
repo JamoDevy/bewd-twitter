0ac806cd97428d0102d8d586b000fe9b50f2631c
h8rry <harrychenca@gmail.com>
2023-05-24 15:39:41 +0800

    Fix by Harry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.create!(user_params)
 
-    def create
-        @user = User.create(
-            username: 'test',
-            email: 'test@test.com',
-            password: 'asdasdasd'
-        )
+    render 'users/create'
+  end
 
-        render 'users/create'
-    end
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
